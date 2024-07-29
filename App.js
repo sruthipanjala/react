4bd8ea6111aca7f891e9a493af5bbcf109d47e79
Panjala Sruthi <143275010+sruthipanjala@users.noreply.github.com>
2024-07-29 23:10:26 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import DatePickerComponent from './DatePickerComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Date Picker Example</h1>
+      <DatePickerComponent />
+    </div>
+  );
+};
+
+export default App;
